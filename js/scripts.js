ad60ce3771fea9d3432826c0a4276e4838f8290d
david rolfs and guy anderson <david@>
2017-04-27 10:59:58 -0700

    create if/else statment with testing alerts

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
   $("form#height").submit(function(){
-    alert("submit works")
+    //alert("submit works")
+    var heightInput = parseInt($("#heightInput").val());
+    //alert(heightInput)
+
+    if(heightInput > 40){
+      alert("You are tall enough to ride!!");
+    }else{
+      alert("sorry you are not tall enough");
+    }
 
 
-  })
-
-
-
-})
+  });
+});
